7333d190dd378c6e469d0f545214f092445d2cbf
wt-854 <wt854@uowmail.edu.au>
2020-06-03 00:00:18 +0800

    Completed form

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -4,9 +4,13 @@ export default (state, action) => {
       return {
         ...state,
         transactions: state.transactions.filter(
-          (transaction) => transaction.id !== action.payload
-        )
-      };
+          (transaction) => transaction.id !== action.payload)
+      }
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions]
+      }
     default:
       return state;
   }

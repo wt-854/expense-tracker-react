c1632e38ef54cf32072d2b5a12219e8ee21fcada
wt-854 <wt854@uowmail.edu.au>
2020-06-03 16:11:58 +0800

    GET method done

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,8 +1,23 @@
+const Transaction = require('../models/Transaction');
+
 // @desc    Get all transactions
 // @route   GET /api/v1/transactions
 // @access  Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    })
+  } catch (err) {
+    return res.send(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
 }
 
 // @desc    Add transaction
